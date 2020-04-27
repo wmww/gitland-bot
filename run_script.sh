e45fa675a7e539df149540a5135f8c24e5d90efb
William Wold <wm@wmww.sh>
2020-04-27 04:37:29 -0400

    Add run script

---
+++ run_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+SERVER_REPO="$PWD"/../gitland/
+CLIENT_REPO="$PWD"/../gitland-client/
+
+iteration() {
+  echo "Pulling server data..."
+  git -C "$SERVER_REPO" pull
+  echo
+  echo Running bot...
+  ./target/release/wmww-gitland-bot --server-repo "$SERVER_REPO" act --client-repo "$CLIENT_REPO"
+  echo
+  echo "Checking for an action..."
+  if ! git -C "$CLIENT_REPO" diff --exit-code act; then
+    git -C "$CLIENT_REPO" add act
+    git -C "$CLIENT_REPO" commit -m "made move"
+    git -C "$CLIENT_REPO" push
+  else
+    echo "No change, still going $(cat $CLIENT_REPO/act)"
+  fi
+}
+
+while true; do
+  iteration
+  echo
+  echo "sleeping..."
+  echo ================================================
+  sleep 45s
+done
