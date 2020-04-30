2e414358f974bd22e4d6a9ed9702685dc1fd1124
William Wold <wm@wmww.sh>
2020-04-30 01:19:52 -0400

    Don't ignore enemies that own the squares around them

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -63,23 +63,22 @@ fn find_target_square(game: &Game) -> Option<(Position, String)> {
         let our_pos = game.our_position();
         for (name, position) in &game.map().players {
             let their_team = game.players[name].team;
-            let they_own_left_of_them =
-                map.controller_of(*position + Position::new(-1, 0)) == their_team;
-            let they_own_top_of_them =
-                map.controller_of(*position + Position::new(0, -1)) == their_team;
+            //let they_own_left_of_them =
+            //    map.controller_of(*position + Position::new(-1, 0)) == their_team;
+            //let they_own_top_of_them =
+            //   map.controller_of(*position + Position::new(0, -1)) == their_team;
             let dist_from_us = our_pos.distance(*position) - DEFEND_RANGE;
             if their_team == game.our_team() || position.x == 0 || position.y == 0 {
                 // Hack until non-active players are kicked
                 continue;
-            } else if position.x > ZONE_SIZE && position.y <= ZONE_SIZE && !they_own_left_of_them {
+            } else if position.x > ZONE_SIZE && position.y <= ZONE_SIZE {
                 let dist = dist_from_us + position.x - ZONE_SIZE;
                 threats.push((dist, Position::new(ZONE_SIZE, position.y), name));
-            } else if position.x <= ZONE_SIZE && position.y > ZONE_SIZE && !they_own_top_of_them {
+            } else if position.x <= ZONE_SIZE && position.y > ZONE_SIZE {
                 let dist = dist_from_us + position.y - ZONE_SIZE;
                 threats.push((dist, Position::new(position.x, ZONE_SIZE), name));
             } else if position.x > ZONE_SIZE
                 && position.y > ZONE_SIZE
-                && (!they_own_left_of_them || !they_own_top_of_them)
             {
                 let dist = dist_from_us + (position.x - ZONE_SIZE) + (position.y - ZONE_SIZE);
                 threats.push((dist, Position::new(ZONE_SIZE, ZONE_SIZE), name));
